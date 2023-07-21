baf486bb525bb1dcee4cd3de16b7f12526f9b5de
AnzhelaPopova <new.daisy@mail.ru>
2023-07-21 16:08:29 +0300

    dropdown

--- my-app/src/components/Body/Body.jsx
+++ my-app/src/components/Body/Body.jsx
@@ -1,112 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import bin from '../../assets/images/bin.png';
 import './Body.css';
-
-
 function Body(props) {
-  const [word, setWord] = useState('');
+  const [dropdown, setDropdown] = useState('');
   const [transcription, setTranscription] = useState('');
   const [translation, setTranslation] = useState('');
   const [inputs] = useState([]); 
-  const [addedWords, setAddedWords] = useState(new Set()); // Set для хранения добавленных слов
-  
+  const [addedWords, setAddedWords] = useState(new Set()); 
   console.log (addedWords)
   console.log(addedWords.values().next().value);
-
   const wordChange = event => {
-    setWord(event.target.value);
+    setDropdown(event.target.selectedOptions[0].text);
   };
-
   const save = () => {
-
-
-    let div = document.getElementById("input"); // Получаем контейнер, в котором будут располагаться новые инпуты
-    let transcription= document.getElementById('transcription').value; // Получаем значение поля transcription
-    let translation = document.getElementById('translation').value; // Получаем значение поля transcription
+    let div = document.getElementById("input");
+    let transcription= document.getElementById('transcription').value;
+    let translation = document.getElementById('translation').value; 
     let transcriptionValue = transcription;
     let translationValue = translation;
-
-    if (transcriptionValue === '' && translationValue === '') { 
-      alert ('Ошибка: слово не найдено'); 
-      return; // Останавливаем выполнение функции 
-  } 
-
-  if (word === '') { 
-      alert ('Ошибка: введите слово'); 
-      return; // Останавливаем выполнение функции 
-  }
-
-  if (addedWords.has(word)) {  
-    alert('Ошибка: слово уже добавлено');  
-    return; // Останавливаем выполнение функции  
-  }
-
-    let newInput1 = document.createElement("input"); // Создаем новый инпут для слова
-    let newInput2 = document.createElement("input"); // Создаем новый инпут для транскрипции
-    let newInput3 = document.createElement("input"); // Создаем новый инпут для перевода
-    let newInput4 = document.createElement("img"); // Создаем новый инпут для перевода
-
-    newInput1.value = word; 
+    let newInput1 = document.createElement("input"); 
+    let newInput2 = document.createElement("input"); 
+    let newInput3 = document.createElement("input"); 
+    let newInput4 = document.createElement("img"); 
+    newInput1.value = dropdown; 
     newInput2.value = transcriptionValue; 
     newInput3.value = translationValue; 
     newInput4.src = bin
-    newInput4.onclick = () => removeInput(div.getElementsByTagName("input").length - 1);
     div.appendChild(newInput1);
     div.appendChild(newInput2); 
     div.appendChild(newInput3); 
     div.appendChild(newInput4);
-
-    setAddedWords(new Set(addedWords.add({ word, transcription: transcriptionValue, translation: translationValue }))); // Добавляем новое слово в Set добавленных слов
-
-
-    setWord(''); 
+    setAddedWords(new Set(addedWords.add({ dropdown, transcription: transcriptionValue, translation: translationValue }))); 
+    setDropdown('');
     setTranscription(''); 
     setTranslation(''); 
   };
-
   useEffect(() => {
-
       localStorage.setItem('addedWords', JSON.stringify(Array.from(addedWords)));
   }, [addedWords]);
-  console.log (localStorage)
-  console.log (addedWords)
-
-
-  const removeInput = (index) => {
-
-    let div = document.getElementById("input");
-    let inputs = div.getElementsByTagName("input");
-    let img = div.getElementsByTagName("img")[index];
-    let removedWord = inputs[index].value; 
-    div.removeChild(img);
-    for (let i = 0; i < inputs.length; i++) {
-      if (i === index || i === index+1 || i === index+2) {
-        div.removeChild(inputs[i]);
-      }
-    }
-    addedWords.delete(removedWord); // Удаляем удаленное слово из Set добавленных слов
-  };
-
+  
   const fetchData = async () => {
     try {
       const response = await fetch('http://itgirlschool.justmakeit.ru/api/words');
       const data = await response.json();
-      let InputWord = document.getElementById('word')
-      let valueWord = InputWord.value
-
-      let wordFound = false;
-
-      for (let i = 0; i < data.length; i++) {
-        if (valueWord === data[i].english) {
-          document.getElementById('transcription').value = data[i].transcription
-          document.getElementById('translation').value = data[i].russian
-          wordFound = true;
-          break; 
-        } 
+  
+      let selectedWord = data.find(option => option.english === dropdown);
+      if (selectedWord) {
+        setTranscription(selectedWord.transcription);
+        setTranslation(selectedWord.russian);
       }
-      if (!wordFound) {
-        alert('такого слова нет в базе данных');
-      }
+  
+      let InputDropdown = document.getElementById('dropdown');
+      data.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option.id;
+        optionElement.textContent = option.english;
+        InputDropdown.appendChild(optionElement);
+      });
     } catch (error) {
       console.log('Ошибка при получении данных:', error);
     }
@@ -114,20 +64,17 @@ function Body(props) {
 
   useEffect(() => {
     fetchData();
-
-  }, []);
+  });
 
   return (
-   
     <div id='input'>
-      <input
-        id='word'
+    <select id="dropdown"
         type="text"
-        value={word}
+        value={dropdown}
         onChange={wordChange}
-        placeholder="Введите слово"
-      />
-
+        placeholder="Введите слово">
+        <option value="">Выберите вариант</option>
+    </select>
       <input
         id='transcription'
         type="text"
@@ -136,7 +83,6 @@ function Body(props) {
         placeholder="Транскрипция"
         readOnly 
       />
-
       <input
         id='translation'
         type="text"
@@ -145,17 +91,11 @@ function Body(props) {
         placeholder="Перевод"
         readOnly 
       />
-
-      <button id="button" onClick={fetchData}>Поиск</button>
       <button id="button" onClick={save}>Сохранить</button>
-
-   {/* Выводим созданные элементы input */}
    {inputs.map((el, index) => (
         <div key={index}>{el}</div>
       ))}
-
     </div>
   );
 }
-
 export default Body;
